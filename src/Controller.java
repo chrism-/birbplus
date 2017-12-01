7f704db4e3769df48749d2d86382f31c9761dd4a
Jason Chang <34146040+JasChang1@users.noreply.github.com>
2017-12-02 09:20:13 +1100

    Fixed addNewSubProject and groupProjectDetails screens
    Screens now actually display

--- src/Controller.java
+++ src/Controller.java
@@ -7,9 +7,22 @@ import javafx.stage.Stage;
 public class Controller {
 	@FXML
     private void handleLogin() throws Exception {
-		Parent window2 = FXMLLoader.load(getClass().getResource("list.fxml"));
+		Parent window2 = FXMLLoader.load(getClass().getResource("groupProjectDetails.fxml"));
 		Stage listStage = new Stage();
 		listStage.setScene(new Scene(window2));
 		listStage.show();
+	}
+	
+	@FXML
+	private void handleAddProject() throws Exception {
+		Parent window3 = FXMLLoader.load(getClass().getResource("addNewSubProject.fxml"));
+		Stage addStage = new Stage();
+		addStage.setScene(new Scene(window3));
+		addStage.show();
+	}
+	
+	@FXML
+	private void handleCloseWindow() throws Exception {
+		
 	}
 }
