5c57f71e72604e41490f3e698074eb8be7b53d42
Jason Chang <34146040+JasChang1@users.noreply.github.com>
2017-12-02 10:57:15 +1100

    Added some random API stuff

--- src/BirbPlusApplication.java
+++ src/BirbPlusApplication.java
@@ -4,12 +4,36 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class BirbPlusApplication extends Application {
 	
-	public static void main(String[] args) {
+	private static String USER_AGENT = "Mozilla/5.0";
+	private static String POST_PARAM = "userName=";
+	
+	public static void main(String[] args) throws Exception {
+		sendPost();
         launch(args);
     }
+	
+	// https://www.journaldev.com/7148/java-httpurlconnection-example-java-http-request-get-post
+	public static void sendPost() throws Exception {
+		URL url = new URL("https://www.freelancer-sandbox.com/api/");
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+		
+		con.setDoOutput(true);
+		OutputStream os = con.getOutputStream();
+		os.write(POST_PARAM.getBytes());
+		os.flush();
+		os.close();
+		
+	}
 	
 	@Override
 	public void start(Stage loginStage) throws Exception {
