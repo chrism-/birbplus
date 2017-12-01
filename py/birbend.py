d1d4be588d0e58876e2fb134e0f6e1e538de1916
chrism- <chrism-@users.noreply.github.com>
2017-12-01 21:08:22 +1100

    first backend commit

--- py/birbend.py
+++ py/birbend.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, Response, json
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +12,26 @@ def api_articles():
 @app.route('/articles/<articleid>')
 def api_article(articleid):
     return 'You are reading ' + articleid
+
+@app.route('/hello', methods = ['GET'])
+def api_hello():
+    data = {
+        'hello'  : 'world',
+        'number' : 3
+    }
+    js = json.dumps(data)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+
+    return resp
+
+def get_user_projects(user):
+    return
+
+def get_user_project_id(id):
+    return
+
 
 if __name__ == '__main__':
     app.run()
